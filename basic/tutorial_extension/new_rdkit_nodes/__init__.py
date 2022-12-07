072bdf323d8e23e996608b9fb1d70bf2c946e643
Greg Landrum <greg.landrum@gmail.com>
2022-12-07 06:59:04 +0100

    rename

--- basic/tutorial_extension/new_rdkit_nodes/__init__.py
+++ basic/tutorial_extension/new_rdkit_nodes/__init__.py
@@ -1,7 +1,7 @@
 # import new_rdkit_nodes.NumAtoms
 # import new_rdkit_nodes.MolFormula
 # import new_rdkit_nodes.rgroup_highlight
-import new_rdkit_nodes.molecule_standardizer
+import new_rdkit_nodes.standardizer_parent
 #import new_rdkit_nodes.visualize_logp_contributions
 #import new_rdkit_nodes.visualize_morganfp_bits
 #import new_rdkit_nodes.visualize_rdkitfp_bits
