f8d22ba826a696c2ec7a66e171c175477e9fadc1
Greg Landrum <greg.landrum@gmail.com>
2022-12-07 06:30:54 +0100

    run yapf

--- basic/tutorial_extension/new_rdkit_nodes/molecule_standardizer.py
+++ basic/tutorial_extension/new_rdkit_nodes/molecule_standardizer.py
@@ -41,7 +41,6 @@
 #  may freely choose the license terms applicable to such Node, including
 #  when such Node is propagated with or for interoperation with KNIME.
 # ------------------------------------------------------------------------
-
 """
 Part of the RDKit Python extension. Node 'Get Parent Molecule (RDKit)'.
 
@@ -54,7 +53,7 @@ import logging
 import knime.extension as knext
 from rdkit import Chem
 import pandas as pd
-import knime.types.chemistry as cet # To work with and compare against chemical data types like SMILES,...
+import knime.types.chemistry as cet  # To work with and compare against chemical data types like SMILES,...
 import pyarrow as pa
 from new_rdkit_nodes import utils
 #import knime_arrow_pandas  # TODO Refactor once ticket AP-19209 is implemented
@@ -73,6 +72,7 @@ rdDepictor.SetPreferCoordGen(True)
 
 LOGGER = logging.getLogger(__name__)
 
+
 @knext.node(
     name="Get Parent Molecule (RDKit)",
     node_type=knext.NodeType.MANIPULATOR,
@@ -80,13 +80,13 @@ LOGGER = logging.getLogger(__name__)
     category=utils.category,
 )
 @knext.input_table(name="Input Data", description="Input molecule in SMILES")
-@knext.output_table(name="Output Data", description="Parent molecule in SMILES")
+@knext.output_table(name="Output Data",
+                    description="Parent molecule in SMILES")
 class GetParentMoleculeNode(knext.PythonNode):
     """Returns standardized parent structure of a molecule.
 
     This node returns the standardized parent structure of an input molecule.
     """
-
 
     action_description = """Standardization action to generate the parent structure:\n
     Largest fragment: Returns the largest fragment after doing a cleanup\n
@@ -107,7 +107,8 @@ class GetParentMoleculeNode(knext.PythonNode):
 
     molecule_column_param = knext.ColumnParameter(
         label="Molecule column",
-        description="Select the molecule column to standardize. The column has to be SMILES or RDKit molecule.",
+        description=
+        "Select the molecule column to standardize. The column has to be SMILES or RDKit molecule.",
         port_index=0,
         column_filter=utils.column_is_convertible_to_mol,
         include_row_key=False,
@@ -124,27 +125,24 @@ class GetParentMoleculeNode(knext.PythonNode):
     def configure(self, configure_context, input_schema_1: knext.Schema):
         # return None
         return input_schema_1.append(
-            knext.Column(
-                ktype=cet.SmilesValue, name="Parent Molecule (SMILES)"
-            )).append(
-knext.Column(
-                ktype=Chem.Mol, name="Parent Molecule"
-            )
-            
-        )
+            knext.Column(ktype=cet.SmilesValue,
+                         name="Parent Molecule (SMILES)")).append(
+                             knext.Column(ktype=Chem.Mol,
+                                          name="Parent Molecule"))
 
-    def execute(self, exec_context: knext.ExecutionContext, input_1: knext.Table):
+    def execute(self, exec_context: knext.ExecutionContext,
+                input_1: knext.Table):
         if self.molecule_column_param is None:
             raise AttributeError(
-                "Molecule column was not selected in configuration dialog."
-            )
+                "Molecule column was not selected in configuration dialog.")
 
         # Prepare mols: If input column consists of rdkit molecules, use them;
         # if input column consists of SMILES, convert to rdkit molecules
         molecule_column_type = input_1.schema[self.molecule_column_param].ktype
         mols = []
         df = input_1.to_pandas()
-        mols = utils.convert_column_to_rdkit_mol(df,molecule_column_type, self.molecule_column_param)
+        mols = utils.convert_column_to_rdkit_mol(df, molecule_column_type,
+                                                 self.molecule_column_param)
 
         progress = 0.0
         add_to_progress = 1 / input_1.num_rows
@@ -152,25 +150,22 @@ knext.Column(
         pmols = []
         for mol in mols:
             if self.stand_action_param == "Largest fragment":
-                    parent = Chem.MolToSmiles(
-                        rdMolStandardize.FragmentParent(mol, skipStandardize=True)
-                    )
+                parent = Chem.MolToSmiles(
+                    rdMolStandardize.FragmentParent(mol, skipStandardize=True))
             elif self.stand_action_param == "Remove charge":
-                    parent = Chem.MolToSmiles(
-                        rdMolStandardize.ChargeParent(mol, skipStandardize=True)
-                    )
+                parent = Chem.MolToSmiles(
+                    rdMolStandardize.ChargeParent(mol, skipStandardize=True))
             elif self.stand_action_param == "Canonical tautomer":
-                    parent = Chem.MolToSmiles(
-                        rdMolStandardize.TautomerParent(mol, skipStandardize=True)
-                    )
+                parent = Chem.MolToSmiles(
+                    rdMolStandardize.TautomerParent(mol, skipStandardize=True))
             elif self.stand_action_param == "Remove istopes":
-                    parent = Chem.MolToSmiles(rdMolStandardize.IsotopeParent(mol))
+                parent = Chem.MolToSmiles(rdMolStandardize.IsotopeParent(mol))
             elif self.stand_action_param == "Remove stereo":
-                    parent = Chem.MolToSmiles(rdMolStandardize.StereoParent(mol))
+                parent = Chem.MolToSmiles(rdMolStandardize.StereoParent(mol))
             elif self.stand_action_param == "Super parent":
-                    parent = Chem.MolToSmiles(rdMolStandardize.SuperParent(mol))
+                parent = Chem.MolToSmiles(rdMolStandardize.SuperParent(mol))
             else:
-                    print("Invalid input")
+                print("Invalid input")
             parents.append(cet.SmilesValue(parent))
             pmols.append(Chem.MolFromSmiles(parent))
             progress += add_to_progress
--- basic/tutorial_extension/new_rdkit_nodes/utils.py
+++ basic/tutorial_extension/new_rdkit_nodes/utils.py
@@ -1,35 +1,47 @@
 import knime_extension as knext
 from rdkit import Chem
-import knime.types.chemistry as cet # To work with and compare against chemical data types like SMILES,...
+import knime.types.chemistry as cet  # To work with and compare against chemical data types like SMILES,...
 
-category = knext.category('/community/rdkit', 'python_nodes', 'New RDKit Nodes', 
-        'RDKit nodes which are written in Python (still exploratory)', 
-        icon='category_rdkit.png')
+category = knext.category(
+    '/community/rdkit',
+    'python_nodes',
+    'New RDKit Nodes',
+    'RDKit nodes which are written in Python (still exploratory)',
+    icon='category_rdkit.png')
 
-
-ctabTypes = (knext.logical(cet.SdfValue),
+ctabTypes = (
+    knext.logical(cet.SdfValue),
     knext.logical(cet.SdfAdapterValue),
-    )
-smilesTypes = (knext.logical(cet.SmilesValue),
-    knext.logical(cet.SmilesAdapterValue),)
-rdkitTypes = (knext.logical(Chem.rdchem.Mol),)
-
+)
+smilesTypes = (
+    knext.logical(cet.SmilesValue),
+    knext.logical(cet.SmilesAdapterValue),
+)
+rdkitTypes = (knext.logical(Chem.rdchem.Mol), )
 
 
 def column_is_convertible_to_mol(column: knext.Column):
     c_type = column.ktype
     allowedTypes = smilesTypes + ctabTypes + rdkitTypes
-    
+
     return c_type in allowedTypes
 
-def convert_column_to_rdkit_mol(df,molecule_column_type,molecule_column_param):
+
+def convert_column_to_rdkit_mol(df, molecule_column_type,
+                                molecule_column_param):
     if molecule_column_type in rdkitTypes:
         LOGGER.warning("rdkit mols detected")
         mols = df[molecule_column_param]
     elif molecule_column_type in smilesTypes:
-        mols = [Chem.MolFromSmiles(smi,sanitize=False) for smi in df[molecule_column_param]]
+        mols = [
+            Chem.MolFromSmiles(smi, sanitize=False)
+            for smi in df[molecule_column_param]
+        ]
     elif molecule_column_type in ctabTypes:
-        mols = [Chem.MolFromMolBlock(mb,sanitize=False,removeHs=False) for mb in df[molecule_column_param]]
+        mols = [
+            Chem.MolFromMolBlock(mb, sanitize=False, removeHs=False)
+            for mb in df[molecule_column_param]
+        ]
     else:
         raise ValueError('unrecognized molecule column type')
     return mols
